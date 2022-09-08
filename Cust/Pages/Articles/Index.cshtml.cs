8b8e5a7518c8442a0e59d979472950c1cd02b74a
Sergey <sergekuv@hotmail.com>
2022-09-08 16:19:59 +0300

    Added article pagination and search

--- Cust/Pages/Articles/Index.cshtml.cs
+++ Cust/Pages/Articles/Index.cshtml.cs
@@ -7,26 +7,96 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Cust.Data;
 using Cust.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace Cust.Pages.Articles
 {
     public class IndexModel : PageModel
     {
-        private readonly Cust.Data.CustContext _context;
+        private readonly CustContext _context;
+        private readonly IConfiguration Configuration;
 
-        public IndexModel(Cust.Data.CustContext context)
+        public IndexModel(CustContext context, IConfiguration configuration)
         {
             _context = context;
+            Configuration = configuration;
         }
 
-        public IList<Article> Article { get;set; } = default!;
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+        //public IList<Article> Article { get;set; } = default!;
+        public PaginatedList<Article> Articles { get; set; }
+
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex)
         {
-            if (_context.Article != null)
+            CurrentSort = sortOrder;
+            DateSort = String.IsNullOrEmpty(sortOrder) ? "date_asc" : "";
+
+            if (searchString != null)
             {
-                Article = await _context.Article.Include(a => a.Tags).ToListAsync();
+                pageIndex = 1;
             }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            CurrentFilter = searchString;
+
+            IQueryable<Article> articlesIQ = from a in _context.Articles select a;
+
+            //Filter will work properly with case insensitive DB collation
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                articlesIQ = articlesIQ.Include(a => a.Tags).Where(s => s.Title.Contains(searchString)
+                                       || s.Content.Contains(searchString)
+                                       || s.Tags.Where(t => t.Id.Contains(searchString)).FirstOrDefault() != null);
+            }
+
+            switch (sortOrder)
+            {
+                case "date_asc":
+                    articlesIQ = articlesIQ.Include(a => a.Tags).OrderBy(r => r.LastEditDate);
+                    break;
+                default:
+                    articlesIQ = articlesIQ.Include(a => a.Tags).OrderByDescending(r => r.LastEditDate);
+                    break;
+
+            }
+
+            var pageSize = Configuration.GetValue("PageSize", 4);
+            Articles = await PaginatedList<Article>.CreateAsync(
+                articlesIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
+
+            //Articles = await articlesIQ.AsNoTracking().ToListAsync();
         }
     }
 }
+
+
+// Initial version
+
+//namespace Cust.Pages.Articles
+//{
+//    public class IndexModel : PageModel
+//    {
+//        private readonly Cust.Data.CustContext _context;
+
+//        public IndexModel(Cust.Data.CustContext context)
+//        {
+//            _context = context;
+//        }
+
+//        public IList<Article> Article { get; set; } = default!;
+
+//        public async Task OnGetAsync()
+//        {
+//            if (_context.Article != null)
+//            {
+//                Article = await _context.Article.Include(a => a.Tags).ToListAsync();
+//            }
+//        }
+//    }
+//}
